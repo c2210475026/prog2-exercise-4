51843efbf4de5968dd6234309b4547d4478a0cfc
Alex Messner :) <alexander.messner1@stud.fh-campuswien.ac.at>
2023-06-09 17:52:00 +0200

    Builder complete

--- src/main/java/at/ac/fhcampuswien/fhmdb/datalayer/api/Builder.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/datalayer/api/Builder.java
@@ -2,10 +2,13 @@ package at.ac.fhcampuswien.fhmdb.datalayer.api;
 
 import at.ac.fhcampuswien.fhmdb.datalayer.models.Genre;
 
+import java.util.UUID;
+
 public interface Builder {
     MovieAPIRequestBuilder query(String query);
     MovieAPIRequestBuilder genre(Genre genre);
     MovieAPIRequestBuilder releaseYear(String releaseYear);
     MovieAPIRequestBuilder ratingFrom(String ratingFrom);
+    MovieAPIRequestBuilder id(UUID id);
 
 }
